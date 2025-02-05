061d76f7172595736e8ad8b54c624fc08a6862eb
Balaji Goud Ithagoni <wo878277@myscc.ca>
2025-02-05 12:38:02 -0500

    Added readme

--- MvcMovie/MvcMovie/obj/Release/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/MvcMovie/obj/Release/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be4701d2bf846e641ffe9ce0f8610f1f4679586d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+269d865843441fda0c4f1ac31ba9c35d2204ce24")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
